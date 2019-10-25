ae8ab4748f5a67bf7c66e71c686274a6269f8b2d
Christophe van de Kerchove <christophe.vkerchove@fxinnovation.com>
2019-10-25 16:13:56 -0400

    Fix: Typo

--- examples/default/deploy.tf
+++ examples/default/deploy.tf
@@ -19,7 +19,7 @@ module "eks" {
   iam_role_name       = random_string.this.result
   name                = random_string.this.result
   security_group_name = random_string.this.result
-  subnet_ids          = tolist(data.aws_subnet_ids.default.ids)
+  subnet_ids          = tolist(data.aws_subnet_ids.this.ids)
 }
 
 #####
